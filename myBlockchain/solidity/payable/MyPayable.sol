dd5c43343c4f0615661b18169942b5df85b44e45
designerasun <asuntheawesome@gmail.com>
2022-02-06 15:35:36 +0900

    feat : sending/receiving ether in solidity

--- myBlockchain/solidity/payable/MyPayable.sol
+++ myBlockchain/solidity/payable/MyPayable.sol
@@ -13,7 +13,30 @@ contract MyPayable {
     }
 
     function getBalance() public view returns(uint256) {
-        uint256 amount = address(myAddress).balance ;
+        uint256 amount = address(this).balance ;
         return amount;
     }
+
+    // method to send Ether 1
+    function transferEth(address payable _user) public payable {
+        _user.transfer(msg.value); // msg.value : number of wei sent with the message
+    }
+    
+    // method to send Ether 2
+    function sendEthe(address payable _user) public payable {
+        bool isSent = _user.send(msg.value);
+        require(isSent, "Sending Ether failed");
+    }
+    
+    // method to send Ether 3
+    function callEth(address payable _user) public payable {
+        (bool isSent, ) = _user.call{ value : msg.value }("");
+        require(isSent);
+    }
+
+     // Function to receive Ether. msg.data must be empty
+    receive() external payable {} // receive takes no argument, cannot have returns, should be external
+
+    // Fallback function is called when msg.data is not empty
+    fallback() external payable {}
 }
