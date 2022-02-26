60dff9887a9e0240a8fc34878416f43a668b85f3
designerasun <asuntheawesome@gmail.com>
2022-02-26 22:49:32 +0900

    feat : 장애물 공 x,z 축 랜덤 위치 생성

--- miniProjects/meowRun/Assets/Scripts/InstantiateBalls.cs
+++ miniProjects/meowRun/Assets/Scripts/InstantiateBalls.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class InstantiateBalls : MonoBehaviour
 {
+    // ==================== 변수 세팅 ==================== //
     public GameObject target; // 3D object sphere
     public GameObject ballCreator; // 공 생성 위치
     public int height = 10; // 공 생성 이후 y축 위치 변수
     public int counter = 0; // 공 생성 제한 변수
-     float invokeTime = 2; // 초기 함수 호출 텀
-     float repeatTime = 0.5f; // 호출 텀 재설정 
+    public float invokeTime = 2; // 초기 함수 호출 텀
+    public float repeatTime = 0.5f; // 호출 텀 재설정 
+    // 난수 생성 범위
+    public float coverage = 2; 
+
+    // ==================== 변수 세팅 ==================== //
+
     void Awake()
     {
         print("spawnBall executed");
@@ -24,19 +30,21 @@ public class InstantiateBalls : MonoBehaviour
             Instantiate(
             target, 
             new Vector3(
-                // FIX : x, z 좌표 위치 수정
-                ballCreator.transform.position.x, 
+                // coverage : 값이 동일하지 않게 세팅할 것
+                ballCreator.transform.position.x + setRandomAxis(-coverage, coverage),
                 ballCreator.transform.position.y + height, 
-                ballCreator.transform.position.z 
+                ballCreator.transform.position.z + setRandomAxis(-coverage, coverage)
             ), 
             Quaternion.identity);
             counter++; // 카운터 증가
         }
     }
 
-    void setAxis()
+    float setRandomAxis(float _min, float _max)
     {
         // 랜덤 x,z 좌표 생성 로직 추가 
+        float axis = Random.Range(_min, _max); 
+        return axis;
     }
 
 }
