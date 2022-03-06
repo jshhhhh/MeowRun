6ad7cffcb01a003b8866bca4bac8f76ef89a9847
designerasun <asuntheawesome@gmail.com>
2022-03-06 13:36:26 +0900

    feat : enemy 인터페이스, 상태별 함수 추가

--- miniProjects/meowRun/unity/Assets/Scripts/enemies/E_rat.cs
+++ miniProjects/meowRun/unity/Assets/Scripts/enemies/E_rat.cs
@@ -2,17 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class E_rat : MonoBehaviour
+public class E_rat : MonoBehaviour, IEnemyBehavior
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject rat;
+    private Player player; 
+    
+    public IEnemyBehavior.enemyState current; 
+
+    void Awake()
     {
-        
+        player = FindObjectOfType<Player>();
+        print($"player is at : {player.transform.position}");
+        current = IEnemyBehavior.enemyState.Idle;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch(current) 
+        {
+            case IEnemyBehavior.enemyState.Idle : 
+                Idle();
+                break;
+            case IEnemyBehavior.enemyState.Track : 
+                Track();
+                return;
+            case IEnemyBehavior.enemyState.Fire : 
+                Fire();
+                return;
+            case IEnemyBehavior.enemyState.Die : 
+                Die();
+                return;
+            default : 
+                Idle();
+                return;
+        }
+    }
+
+    public void Idle() 
+    {
+        // 플레이어가 탐지 거리 바깥이면 주변 패트롤
+        print("Enemy being idle");
+    }
+    public void Fire() 
+    {
+        print("Enemy found a player, starting to shoot");
+    }
+    public void Track() 
+    {
+        // 플레이어가 일정 거리 이상 좁혀지면 추적 시작
+        print("Enemy detected a player, starting tracking");
+    }
+    public void Die() 
+    {
+        // 플레이어가 밟고 지나가면 죽음
+        print("Enemy died by a player");
+        Destroy(this.gameObject);
     }
 }
