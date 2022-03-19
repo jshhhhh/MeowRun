32e388dd45642d44eae63679af4c070cec05a1db
designerasun <asuntheawesome@gmail.com>
2022-03-20 01:32:39 +0900

    feat: enemyType 상태 추가

--- unity/Assets/Scripts/enemies/E_PARENT.cs
+++ unity/Assets/Scripts/enemies/E_PARENT.cs
@@ -1,3 +1,10 @@
+// Enemy logic flow 
+// 1. 플레이어가 일정 거리(50f) 바깥이면 패트롤링
+// 2. 일정 거리 안으로 들어오면 추적 시작(updateState => updateBehavior)
+// 3. 난이도 별로 논리 상이하게 적용(easy : 충돌 시 플레이어 죽음, intermediate & difficult : 사격)
+// 4. 플레이어가 일정 거리 밖으로 나가면 추적/사격 중단 후 다시 패트롤링
+// 5. 플레이어가 점프해서 누를 경우 enemy 죽음
+
 public interface IEnemyBehavior 
 {
     public enum enemyState
@@ -11,6 +18,14 @@ public interface IEnemyBehavior
     public enum playerDistanceState { 
         TooFar, // can't detect player
         Within // detect player
+    }
+
+    public enum enemyType 
+    {
+        Easy, // (쥐, 개구리)
+        Intermediate, // (뱀, 거미)
+        Difficult, // (벌, 외계인<2종>)
+        Object // (공)
     }
 
     // should implement all the functions from interface
