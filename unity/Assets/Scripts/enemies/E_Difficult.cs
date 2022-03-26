ecbba4919ba37f1b4e6e27e5e5f1c9012e9125fc
designerasun <asuntheawesome@gmail.com>
2022-03-26 16:11:22 +0900

    feat: flying enemy 구현, track 코루틴, rotation 메소드 추가

--- unity/Assets/Scripts/enemies/E_Difficult.cs
+++ unity/Assets/Scripts/enemies/E_Difficult.cs
@@ -3,31 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
+// difficult type enemy does not use NavMesh
+// should track
+// should fire
+
 // Enemy type : Difficult
 public class E_Difficult : MonoBehaviour, IEnemyBehavior
 {
     private Player player; 
     private float distance; // Player ~ enemy 사이 거리
-    private string difficultType;
+    private string difficultType; // enemy 타입
+    private bool shouldFire = false; // intermediate, difficult enemy는 fire 가능
     
-    public IEnemyBehavior.enemyState current; // enemy 상태
-    public IEnemyBehavior.playerDistanceState isDetected; // enemy의 player 탐지  
-    public NavMeshAgent _agent; // enemy 인공지능 인스턴스
-    [SerializeField] Transform[] AgentRoutes;
-    public int routeIndex = 0;
+    private IEnemyBehavior.enemyState current; // enemy 상태
+    private IEnemyBehavior.playerDistanceState isDetected; // enemy의 player 탐지  
+    // public NavMeshAgent _agent; // enemy 인공지능 인스턴스
+    // [SerializeField] Transform[] AgentRoutes;
+    [SerializeField] Transform[] inAirRoutes;
+    private int routeIndex = 0;
+    [SerializeField] float patrolSpeed = 2f;
     
-    public float detectLimit = 5f; // enemy 감지 거리 한계, 에디터에서 설정 가능하도록 세팅
+    [Range (0,15)] [SerializeField] float detectLimit = 5f; // enemy 감지 거리 한계, 에디터에서 설정 가능하도록 세팅
+    [Range (0,15)] [SerializeField] float fireLimit = 2.5f; // enemy 사격 거리 한계, detectLimit보다 작게 설정할 것.
+
 
     // ============== Object initialization and update ============== // 
     void Awake()
     {
-        current = IEnemyBehavior.enemyState.Idle; // awake시 상태는 idle
-        player = FindObjectOfType<Player>();
-        _agent = this.GetComponent<NavMeshAgent>();
-        difficultType = IEnemyBehavior.enemyType.Easy.ToString();
-        _agent.autoBraking = false; // for continuous movement between points
-        isDetected = IEnemyBehavior.playerDistanceState.TooFar;
-        print($"player is at : {player.transform.position}");
+        InitSetup();
     }
 
     void Update()
@@ -35,15 +39,42 @@ public class E_Difficult : MonoBehaviour, IEnemyBehavior
         updateState();
         updateBehavior();
     }
+
+        void InitSetup() 
+    {
+        // 플레이어 & NavMesh 초기화
+        player = FindObjectOfType<Player>(); 
+        // _agent = this.GetComponent<NavMeshAgent>(); 
+
+        // Enemy 초기화 : awake시 상태는 idle, not detectable
+        if (player != null ) // 오브젝트 null check && _agent != null
+        {
+            current = IEnemyBehavior.enemyState.Idle; 
+            isDetected = IEnemyBehavior.playerDistanceState.TooFar;
+            difficultType = IEnemyBehavior.enemyType.Difficult.ToString();
+            // _agent.autoBraking = false; // NavMeshAgent 연속적인 움직임 설정
+        } 
+    }
     // ============== Object initialization and update ============== // 
 
     // ============== Enemy state and behavior ============== // 
     public void calculateDistance() { 
-        // 플레이어가 일정 거리 이상 좁혀지면 추적 시작
+        // 플레이어 ~ 적 거리 계산
         distance = Vector3.Distance(player.transform.position, this.transform.position);
+
+        // enemy 초기 상태 : 사격하지 않음, 추적 멈추지 않음.
+        shouldFire = false;
+
+        //  플레이어가 탐지 거리 바깥이면 패트롤 시작
+        if (distance > detectLimit) isDetected = IEnemyBehavior.playerDistanceState.TooFar;
+
+        // 플레이어가 탐지 거리 이하 좁혀지면 추적 시작
         if (distance < detectLimit) {
             isDetected = IEnemyBehavior.playerDistanceState.Within;
         }
+
+        // 플레이어가 사격 거리 이하 좁혀지면 사격 시작
+        if (distance < fireLimit) shouldFire = true;
     }
     public void updateBehavior()
     {
@@ -53,11 +84,9 @@ public class E_Difficult : MonoBehaviour, IEnemyBehavior
                 Idle();
                 break;
             case IEnemyBehavior.enemyState.Track : 
-                // TO DO : enemy에 작은 신호등 붙이기(track state 표시용)
-                // this.gameObject.add
                 Track();
                 break;
-            case IEnemyBehavior.enemyState.Fire : // Enemy type 중 rat은 fire하지 않음
+            case IEnemyBehavior.enemyState.Fire :
                 Fire();
                 break;
             case IEnemyBehavior.enemyState.Die : 
@@ -71,13 +100,17 @@ public class E_Difficult : MonoBehaviour, IEnemyBehavior
 
     public void updateState()
     {
-        calculateDistance(); // isDetected 변수 상태 change
+        calculateDistance(); // isDetected, shouldFire 변수 상태 change
         switch (isDetected) {
             case IEnemyBehavior.playerDistanceState.TooFar :
                 current = IEnemyBehavior.enemyState.Idle;
                 break;
             case IEnemyBehavior.playerDistanceState.Within : 
-                current = IEnemyBehavior.enemyState.Track;
+                if (shouldFire) {
+                    current = IEnemyBehavior.enemyState.Fire;
+                } else {
+                    current = IEnemyBehavior.enemyState.Track;
+                }
                 break;
             default : 
                 current = IEnemyBehavior.enemyState.Idle;
@@ -98,12 +131,30 @@ public class E_Difficult : MonoBehaviour, IEnemyBehavior
     public void Track() 
     {
         print("Enemy detected a player, starting tracking");
-        _agent.destination = player.transform.position;
+        StartCoroutine(EnemyTrackCoroutine(0.01f));
+    }
+
+    IEnumerator EnemyTrackCoroutine(float time)
+    {
+        // 1. set rotation to player
+        ShouldLookAtPlayer();
+
+        // 2. Track player
+        yield return new WaitForSeconds(time);
+        transform.position = Vector3.MoveTowards(
+                transform.position, 
+                player.transform.position, 
+                Time.deltaTime * patrolSpeed
+        );
+
+        // 3. end coroutine
+        yield return null;
     }
 
     public void Fire() 
     {
         print("Difficult Enemy firing projectiles faster");
+        ShouldLookAtPlayer();
     }
 
     public void Die() 
@@ -115,11 +166,33 @@ public class E_Difficult : MonoBehaviour, IEnemyBehavior
 
     public void Patrol()
     {
-        _agent.destination = AgentRoutes[routeIndex].transform.position;
-        if (!_agent.pathPending && _agent.remainingDistance < 0.5f)
+        // 첫 번째 inAirRoutes로 enemy 이동
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            inAirRoutes[routeIndex].transform.position, 
+            Time.deltaTime * patrolSpeed
+        );
+
+        // inAirRoutes에 맞춰 경로 상에서 반복 패트롤
+        if (Vector3.Distance(transform.position, inAirRoutes[routeIndex].transform.position) < 1f) 
         {
-            routeIndex = (routeIndex+1)%AgentRoutes.Length;
+            routeIndex = (routeIndex+1)%inAirRoutes.Length;
         }
     }
     // ============== IEnemyBehavior implementation ============== // 
+
+    // ============== Enemy rotation implementation ============== // 
+    void ShouldLookAtPlayer()
+    {
+        Vector3 playerPosWithLockedYAxis = new Vector3(
+            player.transform.position.x, 
+            transform.position.y, 
+            player.transform.position.z
+        ); 
+
+        transform.LookAt(playerPosWithLockedYAxis); // fix y axis
+        transform.Rotate( 0, 90, 0 ); // rotate enemy 90 degree in Y axis to correct direction
+    }
+
+    // ============== Enemy rotation implementation ============== // 
 }
