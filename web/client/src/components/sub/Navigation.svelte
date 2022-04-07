caf60394c2c32deb2161a553c13e4d754a15f385
designerasun <asuntheawesome@gmail.com>
2022-04-07 18:03:55 +0900

    feat : 네비게이션 및 스타일 추가

--- web/client/src/components/sub/Navigation.svelte
+++ web/client/src/components/sub/Navigation.svelte
@@ -1,23 +1,42 @@
 <script lang="ts">
-
+const handleClick = () => {
+    // TO DO 
+}
 </script>
 
 
-<main>
-<ul id="navigationBar">
-    <li><a href="#main">Main</a></li>
-    <li><a href="#about">About</a></li>
-    <li><a href="#whyMeowRun">Why MeowRun</a></li>
-    <li><a href="#howItWorks">How It Works</a></li>
-    <li><a href="#characterVote">Character Vote</a></li>
-    <button>Login</button>
-</ul>
+<main id="navigation">
+    <div id="logo">
+        <img src="" alt="logo">
+    </div>
+    <ul id="navigationBar">
+        <li><a href="#main">Main</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#whyMeowRun">Why MeowRun</a></li>
+        <li><a href="#howItWorks">How It Works</a></li>
+        <li><a href="#characterVote">Character Vote</a></li>
+    </ul>
+    <div id="auth">
+        <a href="#loginPage" on:click={handleClick}><button>Login</button></a>
+    </div>
 </main>
 
 
 <style lang="scss">
     @import '../partials/common.scss';
-#navigationBar {
-    @include flexRow();
-}
+    #navigation {
+        @include flexRow();
+        flex-wrap: nowrap;
+        justify-content: space-around;
+        align-items: center;
+        background-color: $layoutColor;
+        color: white;
+        font-size: $unit;
+        margin: 0;
+        padding: 0;
+    }
+    #navigationBar {
+        @include flexRow();
+        gap: $unit;
+    }
 </style>
