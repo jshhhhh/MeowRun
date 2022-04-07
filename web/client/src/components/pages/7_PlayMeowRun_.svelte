b174d210ab7318dff781a2a639f28f712a73de49
designerasun <asuntheawesome@gmail.com>
2022-04-07 18:02:40 +0900

    feat : simmer.io 게임 태그 컴포넌트 추가

---
+++ web/client/src/components/pages/7_PlayMeowRun_.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    
+</script>
+    
+    
+<main id="main">
+    <iframe 
+    title="jumping car"
+    src="https://i.simmer.io/@developerasun/jumpingcar2" 
+    style="width:960px;height:600px;border:0">
+</iframe>
+</main>
+
+
+<style lang="scss">
+
+</style>
