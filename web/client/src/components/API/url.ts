044a04264450d2f8afd9e902eff3ccf8a19d30ab
designerasun <asuntheawesome@gmail.com>
2022-04-07 18:07:03 +0900

    chore : API url 업데이트, sass 스타일 공통 변수 추가

--- web/client/src/components/API/url.ts
+++ web/client/src/components/API/url.ts
@@ -2,6 +2,7 @@ export const API_URL = {
     AUTH: { 
         login: "http://localhost:3001/temp",
         logout: "http://localhost:3001/temp",
-        signup: "http://localhost:3001/temp"
+        signup: "http://localhost:3001/temp",
+        signout: "http://localhost:3001/temp"
     }
 }
