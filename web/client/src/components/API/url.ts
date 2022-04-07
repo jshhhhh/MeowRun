f588e3091540df24e58482efbbfd63b0e7c26749
designerasun <asuntheawesome@gmail.com>
2022-04-07 16:43:58 +0900

    feat: api url object added

---
+++ web/client/src/components/API/url.ts
@@ -0,0 +1,7 @@
+export const API_URL = {
+    AUTH: { 
+        login: "http://localhost:3001/temp",
+        logout: "http://localhost:3001/temp",
+        signup: "http://localhost:3001/temp"
+    }
+}
