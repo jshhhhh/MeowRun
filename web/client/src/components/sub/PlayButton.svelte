1a7309f810ae679330e93a293018dbc2f4a61c23
designerasun <asuntheawesome@gmail.com>
2022-04-08 17:52:41 +0900

    refactor: 버튼 컴포넌트 prop 추가, 스타일 그러데이션 fix

--- web/client/src/components/sub/PlayButton.svelte
+++ web/client/src/components/sub/PlayButton.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
-
+// set props
+export let buttonText;
+export let jumpTo;
 </script>
 
 
-<main>
-    <a href="#playMeowRun"><button>Play MeowRun</button></a>
+<main id="playButton">
+    <a href={`#${jumpTo}`}><button>{buttonText}</button></a>
 </main>
 
 
 <style lang="scss">
-
+    @import '../partials/common.scss';
+    #playButton {
+        button {
+            font-size : $unit;
+            padding : $unit;
+            border-image : linear-gradient(to right, #1B51F2 0%, #F263DA 100%);
+            border-image-slice : 4;
+            border-image-width : 0.2rem;
+            background-color: transparent;
+            color: white;
+            font-weight: bold;
+            cursor: pointer;
+        }
+    }
 </style>
