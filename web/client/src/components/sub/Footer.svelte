1a96526140b3ef78d677b35ea610d9e75b32f2ad
designerasun <asuntheawesome@gmail.com>
2022-04-12 13:42:52 +0900

    feat:모바일 반응형 디자인/스타일 추가

--- web/client/src/components/sub/Footer.svelte
+++ web/client/src/components/sub/Footer.svelte
@@ -7,18 +7,18 @@
 <ul id="project">
     <li class="title">Project</li>
     <ul>
-        <li><a href="https://github.com/DaeguDev/MeowRun">Readme</a></li>
-        <li><a href="https://github.com/DaeguDev/MeowRun/issues">Troubleshooting</a></li>
-        <li><a href="https://github.com/DaeguDev/MeowRun/discussions">Discussion</a></li>
+        <li class="footerItem"><a href="https://github.com/DaeguDev/MeowRun">Readme</a></li>
+        <li class="footerItem"><a href="https://github.com/DaeguDev/MeowRun/issues">Troubleshooting</a></li>
+        <li class="footerItem"><a href="https://github.com/DaeguDev/MeowRun/discussions">Discussion</a></li>
     </ul>
 
 </ul> 
 <ul id="contributor">
     <li class="title">Contributor</li>
     <ul>
-        <li><a href="https://github.com/jshhhhh">Seokhwan Jung</a></li>
-        <li><a href="https://github.com/omago123">Jungi Kim</a></li>
-        <li><a href="https://github.com/developerasun">Jake Sung</a></li>
+        <li class="footerItem"><a href="https://github.com/jshhhhh">Seokhwan Jung</a></li>
+        <li class="footerItem"><a href="https://github.com/omago123">Jungi Kim</a></li>
+        <li class="footerItem"><a href="https://github.com/developerasun">Jake Sung</a></li>
     </ul>
 </ul>
 </footer>
@@ -26,17 +26,33 @@
 
 <style lang="scss">
     @import '../partials/common.scss';
-
-    #footer { 
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
+    #footer {
+        @include flexColumn();
         background-color: $subBgColor;
         text-align: center;
         padding: $unit*1.3;
         .title {
-            font-size: $unit * 1.3;
+            font-size: $unit * 1.6;
             font-weight: bold;
-            padding-bottom: $unit;
+            padding: $unit;
+        }
+        .footerItem {
+            font-size: $unit * 1.2;
+            padding: $unit;
+            border: 2px solid;
+            border-image : linear-gradient(to right, #1B51F2 0%, #F263DA 100%);
+            border-image-slice : 4;
         }
     }
+    @media screen and (min-width:$tablet) {
+        #footer { 
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+        }
+        .footerItem {
+            width:auto;
+        }
+    }
+
+    
 </style>
