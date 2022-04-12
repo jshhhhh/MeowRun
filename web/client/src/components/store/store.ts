b9e3a7acb0511466aa1c6a6b81331237f2f06db2
designerasun <asuntheawesome@gmail.com>
2022-04-12 13:44:38 +0900

    chore:API url 업데이트, sass breakpoint/store 변수 추가

--- web/client/src/components/store/store.ts
+++ web/client/src/components/store/store.ts
@@ -1,6 +1,6 @@
 import { writable } from "svelte/store";
 
-export const tempStoreVariable = writable(0)
 export const username = writable('')
+export const storeVoteCount = writable(0)
 
 // TO DO: add stores for Character votes: Garfield, FishCat, and Tricolor
