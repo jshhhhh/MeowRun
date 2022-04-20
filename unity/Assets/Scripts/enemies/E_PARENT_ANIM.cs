621b743bd10958dfe1c5160fae0359751542e56c
designerasun <asuntheawesome@gmail.com>
2022-04-20 15:34:42 +0900

    feat: 애니메이션 파라미터 enum 추가

--- unity/Assets/Scripts/enemies/E_PARENT_ANIM.cs
+++ unity/Assets/Scripts/enemies/E_PARENT_ANIM.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 public interface IEnemyAnimation 
 {
+    // below parameters are corresponding to the ones in Animator component
+    public enum Parameters {
+        IDLE, 
+        TRACK,
+        ATTACK, 
+        DIE,
+        IDLE_OR_TRACK,
+    }
+
     void IdleAnim();
     void TrackAnim();
+    void IdleOrTrackAnim();
     void AttackAnim();
     void DieAnim();
     void AnimationSetter<T>(string _name, T _condition);
